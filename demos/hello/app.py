83eae4afc5a5ca050d360fd3e3b7ba2663bf14b8
zengtaozt <100819147@qq.com>
2018-12-04 00:44:08 +0800

    Apllication hello done

--- demos/hello/app.py
+++ demos/hello/app.py
@@ -19,12 +19,23 @@ def index():
 
 # bind multiple URL for one view function
 
+@app.route('/hi')
+@app.route('/hello')
+def say_hello():
+    return '<h1>Hello Flask</h1>'
 
 
 # dynamic route, URL variable default
 
-
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name = 'Programmer'):
+    return '<h1>Hello, %s!</h1>'%name
 
 # custom flask cli command
+
+@app.cli.command()
+def hello():
+    click.echo('Hello, Human!')
 
 
